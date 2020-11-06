0d3749aba8e0aef8d4c722726c51393ed10d5d17
MacksonWelton <mackson.welton@gmail.com>
2020-11-06 08:46:25 -0300

    Updates

---
+++ precache-manifest.2ea3ffeebfc8df7e1b0e3f4286f393ce.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4ff3da092f40d59a2b9e2440312511",
+    "url": "/novo-painel-colaborador-secretaria/index.html"
+  },
+  {
+    "revision": "f00794ef95835a249e07",
+    "url": "/novo-painel-colaborador-secretaria/static/css/2.b472b2fd.chunk.css"
+  },
+  {
+    "revision": "032a8f24bf7e9ad05484",
+    "url": "/novo-painel-colaborador-secretaria/static/css/main.c4e9001a.chunk.css"
+  },
+  {
+    "revision": "f00794ef95835a249e07",
+    "url": "/novo-painel-colaborador-secretaria/static/js/2.4cf05798.chunk.js"
+  },
+  {
+    "revision": "eb998825b368fb48328883d1b978652a",
+    "url": "/novo-painel-colaborador-secretaria/static/js/2.4cf05798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "032a8f24bf7e9ad05484",
+    "url": "/novo-painel-colaborador-secretaria/static/js/main.1dbc5935.chunk.js"
+  },
+  {
+    "revision": "8d0d562f4ec3c131ac27d52de65ecd17",
+    "url": "/novo-painel-colaborador-secretaria/static/js/main.1dbc5935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78de777bb23cb824f088",
+    "url": "/novo-painel-colaborador-secretaria/static/js/runtime-main.86a66c0e.js"
+  },
+  {
+    "revision": "f38ddea9dedadea03c5d43c596dac13f",
+    "url": "/novo-painel-colaborador-secretaria/static/media/argon-react.f38ddea9.png"
+  },
+  {
+    "revision": "067595ad77ecc0db9c81c8905a7eef32",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-brands-400.067595ad.woff2"
+  },
+  {
+    "revision": "57dcda6f368ea90179f75cbdae96c263",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-brands-400.57dcda6f.eot"
+  },
+  {
+    "revision": "9d67fa1429375bd2a899a17eb77d0342",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-brands-400.9d67fa14.svg"
+  },
+  {
+    "revision": "9ec698d1a597bff5df337094b71ddaaf",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-brands-400.9ec698d1.ttf"
+  },
+  {
+    "revision": "b564da88bbf0c4aa446fa19653713cd1",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-brands-400.b564da88.woff"
+  },
+  {
+    "revision": "3351f435b3c9037fd88aeb04dc1e43bc",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-regular-400.3351f435.eot"
+  },
+  {
+    "revision": "4165c2688309cbfb1b877caf8f75afb5",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-regular-400.4165c268.woff2"
+  },
+  {
+    "revision": "5d0861781aeef6c82fda3a3076954a1b",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-regular-400.5d086178.svg"
+  },
+  {
+    "revision": "73cf49a2232c06c920b7a34e36bfb58c",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-regular-400.73cf49a2.woff"
+  },
+  {
+    "revision": "a0e3ac82940c1998c5977fd4bc1f5ef6",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-regular-400.a0e3ac82.ttf"
+  },
+  {
+    "revision": "0724bb8b89ab6b8b9b7df917b17be0b7",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-solid-900.0724bb8b.svg"
+  },
+  {
+    "revision": "55eb2a60e8181f0e68b558c991973bf0",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-solid-900.55eb2a60.woff2"
+  },
+  {
+    "revision": "75f38a159982b6bd1704891332d95fa7",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-solid-900.75f38a15.ttf"
+  },
+  {
+    "revision": "89e02bae13c9131c7468b1e729339ac1",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-solid-900.89e02bae.eot"
+  },
+  {
+    "revision": "cdfec5cf5e9840889790bcf2c4042583",
+    "url": "/novo-painel-colaborador-secretaria/static/media/fa-solid-900.cdfec5cf.woff"
+  },
+  {
+    "revision": "2fb66b88a043dbdcb51a351431f84d04",
+    "url": "/novo-painel-colaborador-secretaria/static/media/facebook.2fb66b88.svg"
+  },
+  {
+    "revision": "87be59a1f7061fc6021876aad6fee028",
+    "url": "/novo-painel-colaborador-secretaria/static/media/google.87be59a1.svg"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/novo-painel-colaborador-secretaria/static/media/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/novo-painel-colaborador-secretaria/static/media/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/novo-painel-colaborador-secretaria/static/media/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/novo-painel-colaborador-secretaria/static/media/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/novo-painel-colaborador-secretaria/static/media/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "e2582654b8e7b6c285d5017df6427d73",
+    "url": "/novo-painel-colaborador-secretaria/static/media/profile-cover.e2582654.jpg"
+  },
+  {
+    "revision": "0cd7c7ccf9fc75166057f21eed1be46e",
+    "url": "/novo-painel-colaborador-secretaria/static/media/sistemalogo.0cd7c7cc.png"
+  },
+  {
+    "revision": "53033970a416368da35794389680266f",
+    "url": "/novo-painel-colaborador-secretaria/static/media/team-1-800x800.53033970.jpg"
+  },
+  {
+    "revision": "497bb3590e24c9f8b645864bfffc39b8",
+    "url": "/novo-painel-colaborador-secretaria/static/media/team-3-800x800.497bb359.jpg"
+  },
+  {
+    "revision": "230071328b705f8686cabd26a85ed6a5",
+    "url": "/novo-painel-colaborador-secretaria/static/media/team-4-800x800.23007132.jpg"
+  }
+]);
